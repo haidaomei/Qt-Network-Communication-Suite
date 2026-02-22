32cd89139fd8f839f72da386fb6febcfd2cdff1b
haidaomei <DDdaomei@hotmail.com>
2026-02-23 07:18:17 +0800

    Refactor Server.h to remove commented-out code

--- NetworkCommunicationServer/Server.h
+++ NetworkCommunicationServer/Server.h
@@ -16,7 +16,7 @@ private:
     int ListeningPort;
 
     QHash<QString,QTcpSocket*> NameToSocketTable;
-    QHash<QTcpSocket*,QString> SocketToNameTable;//双向,方便查找
+    //QHash<QTcpSocket*,QString> SocketToNameTable;//双向,方便查找
 
 public:
     Server(QObject* parent=nullptr);//详见gof《设计模式》组合模式,有别于其他设计模式,比如oop最基本的工厂模式、抽象工厂模式、单例模式,godot的设计模式也是这种(听说unity也是,日后可以开一篇笔记继续讨论)
@@ -35,3 +35,4 @@ private slots://负责处理被connect信号并触发信号后该进行的逻辑
 };
 
 #endif // SERVER_H
+
